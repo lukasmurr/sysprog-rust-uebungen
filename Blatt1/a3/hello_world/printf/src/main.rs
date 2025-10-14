c59346df8568f14263d25ad743502cee93ca262e
Lukas Murr <120203386+lukasmurr@users.noreply.github.com>
2025-10-14 13:53:47 +0200

    Fix clippy erros

--- Blatt1/a3/hello_world/printf/src/main.rs
+++ Blatt1/a3/hello_world/printf/src/main.rs
@@ -1,6 +1,6 @@
 use std::fmt;
+
 #[allow(clippy::print_literal)]
-#[deny(clippy::approx_constant)]
 fn main() {
     // In general, the `{}` will be automatically replaced with any
     // arguments. These will be stringified.
--- Blatt1/a3/primitives/literals/src/main.rs
+++ Blatt1/a3/primitives/literals/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     // Integer subtraction
     println!("1 - 2 = {}", 1i32 - 2);
     // println!("1 - 2 = {}", 1u32 - 2); attempt to compute `1_u32 - 2_u32`, which would overflow
-    
+
     // Scientific notation
     println!("1e4 is {}, -2.5e-3 is {}", 1e4, -2.5e-3);
 
