7ff585ac2fc33f67ba2d822492a45940bb60eab0
Lukas Murr <lukas.murr@bauernshop.de>
2025-12-01 09:43:34 +0100

    Update integration_test.rs

--- Blatt8/a2/catsay/tests/integration_test.rs
+++ Blatt8/a2/catsay/tests/integration_test.rs
@@ -1,6 +1,6 @@
 use assert_cmd::prelude::*; // Add methods on commands
 use predicates::prelude::*;
-use std::process::Command; // Run programs // Used for writing assertions
+use assert_cmd::Command; // Run programs // Used for writing assertions
 
 #[test]
 fn run_with_defaults() -> Result<(), Box<dyn std::error::Error>> {
@@ -16,7 +16,7 @@ fn run_with_defaults() -> Result<(), Box<dyn std::error::Error>> {
 fn fail_on_non_existing_file() -> Result<(), Box<dyn std::error::Error>> {
     Command::cargo_bin("catsay")
         .expect("binary exists")
-        .args(&["-f", "no/such/file.txt"])
+        .args(["-f", "no/such/file.txt"])
         .assert()
         .failure();
     Ok(())
