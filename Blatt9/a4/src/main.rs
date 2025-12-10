7c5e429e9c49629209dd4bd02fb9991321451862
Lukas Murr <lukas.murr@bauernshop.de>
2025-12-10 13:24:27 +0100

    Update main.rs

--- Blatt9/a4/src/main.rs
+++ Blatt9/a4/src/main.rs
@@ -4,7 +4,6 @@ use serde::Deserialize;
 use std::fs;
 use std::sync::{Arc, Mutex};
 use std::thread;
-use std::time::Duration;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -21,7 +20,7 @@ struct Config {
     transfers_pro_thread: usize,
 }
 
-fn ueberweisung(konten: &Vec<Mutex<i32>>, von: usize, nach: usize, betrag: i32) {
+fn ueberweisung(konten: &[Mutex<i32>], von: usize, nach: usize, betrag: i32) {
     if von == nach {
         return;
     }
@@ -31,37 +30,12 @@ fn ueberweisung(konten: &Vec<Mutex<i32>>, von: usize, nach: usize, betrag: i32) 
 
     let mut first_guard = konten[first_lock_idx].lock().unwrap();
     let mut second_guard = konten[second_lock_idx].lock().unwrap();
-
-    // Now we have both locks, we can safely perform the transfer.
-    // We need to be careful about which guard corresponds to 'von' and 'nach'.
-    // Since we have mutable references to the data inside the mutexes, we can just modify them.
-    // However, we need to know which value belongs to 'von' and which to 'nach'.
-
-    // To make it cleaner, let's just modify the values directly via the guards.
-    // But wait, first_guard is for first_lock_idx.
-
-    let von_balance = if von == first_lock_idx {
-        &mut *first_guard
-    } else {
-        &mut *second_guard
-    };
-
-    // We can't borrow from both guards simultaneously if we do it like above because of lifetimes/borrow checker potentially?
-    // Actually, we can. But let's do the check logic first.
-
-    // We need to check if 'von' has enough money.
-    // But we can't easily get 'von_balance' and 'nach_balance' as mutable references at the same time
-    // if we try to do it conditionally in a complex way.
-
-    // Let's do it simply:
     if von == first_lock_idx {
-        // first_guard is 'von', second_guard is 'nach'
         if *first_guard >= betrag {
-            *first_guard -= betrag;
+            *first_guard -= betrag
             *second_guard += betrag;
         }
     } else {
-        // first_guard is 'nach', second_guard is 'von'
         if *second_guard >= betrag {
             *second_guard -= betrag;
             *first_guard += betrag;
@@ -94,7 +68,7 @@ fn main() {
 
     println!("Total start amount: {}", start_geld);
 
-    for t_id in 0..config.threads {
+    for _ in 0..config.threads {
         let konten_clone = Arc::clone(&konten);
         let transfers = config.transfers_pro_thread;
         let num_konten = config.num_konten;
@@ -118,9 +92,8 @@ fn main() {
 
     let mut end_geld = 0;
     println!("---------------------");
-    for (i, konto) in konten.iter().enumerate() {
+    for (konto) in konten.iter().enumerate() {
         let saldo = *konto.lock().unwrap();
-        // println!("Konto #{:03}: {:7}", i, saldo); // Optional: print all accounts
         end_geld += saldo;
     }
 
